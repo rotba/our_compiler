979eed9b460021ba79aa6d93f6bd0324443ef7e6
rotba <rotba@post.bgu.ac.il>
2019-12-15 10:06:59 +0200

    all test pass

--- reader.ml
+++ reader.ml
@@ -278,7 +278,9 @@ let tok_sym =
 
 let rec dotted_list_list_packer = function
   | (e::[], (d, s)) -> Pair(e, s)
-  | (e::s, f) -> Pair(e, (dotted_list_list_packer (s, f)));;
+  | (e::s, f) -> Pair(e, (dotted_list_list_packer (s, f)))
+  |([],_)-> raise Exhausting
+;;
 
 let nt_float_to_string = 
   PC.pack nt_float ( fun (x) ->
