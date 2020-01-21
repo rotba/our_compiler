61b5088bbc0e9030382178a87f5476ecaed486d4
rotba <rotba@post.bgu.ac.il>
2020-01-21 13:19:57 +0200

    all our tests pass with stdlib

--- compiler.ml
+++ compiler.ml
@@ -140,7 +140,7 @@ exception X_missing_input_file;;
 
 try
   let infile = Sys.argv.(1) in
-  let code =  (* (file_to_string "stdlib.scm") ^ *) (file_to_string infile) in
+  let code =  (file_to_string "stdlib.scm") ^ (file_to_string infile) in
   let asts = string_to_asts code in
   let asts = rename asts in
   let consts_tbl = Code_Gen.make_consts_tbl asts in
