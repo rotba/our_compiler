86370af2ce64b4732df5f6735d206dcc89b84d41
ABraverman <alonybraverman@gmail.com>
2020-01-23 22:23:56 +0200

    Auto stash before merge of "master" and "origin/master"

--- test.scm
+++ test.scm
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
 (if (equal? '((1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
  24 25 26)) '((1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26))) 'PASSED! '(Excpected Result: (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
  24 25 26),     Actual Result: (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26),    testcase: TESTCASE_35) )
+=======
+(if (equal? '(-15) '(-7350434)) 'PASSED! '(Excpected Result: -15,     Actual Result: -7350434,    testcase: TESTCASE_12) )
+>>>>>>> Stashed changes
--- testcases/last_year/TESTCASE_25.scm
+++
@@ -1,5 +0,0 @@
-(define a (make-vector 5 2))
-(define (foo i)
-          (* (vector-ref a i) i ...
-          
-(apply + `(,(foo 0) ,(foo 1) ,(foo 2) ,(foo 3) ,(foo 4)))
